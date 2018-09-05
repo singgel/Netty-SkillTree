0c54372400219cc1f3a89dd77ab1d4785830f313
heks <heks@yiche.com>
2018-09-05 19:58:29 +0800

    事情越来越多，规范点吧，接下来是Hadoop的

---
+++ src/test/java/com/singgel/hadooprpc/ClientProtocol.java
@@ -0,0 +1,10 @@
+package com.singgel.hadooprpc;
+
+import java.io.IOException;
+
+interface ClientProtocol extends VersionedProtocol {
+    // 版本号,默认情况下,不同版本号的 RPC Client 和 Server 之间不能相互通信
+    public static final long versionID = 1L;
+    String echo(String value) throws IOException;
+    int add(int v1, int v2) throws IOException;
+}
